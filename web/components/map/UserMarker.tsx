2e7af039b6adc4867d48e89edf0c74f24cd74d57
BoscoNg16 <bosco.c.ng@gmail.com>
2026-01-24 21:29:05 -0500

    Current waypoint

--- web/components/map/UserMarker.tsx
+++ web/components/map/UserMarker.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Marker, Popup } from "react-leaflet";
-import { Icon } from "leaflet";
-import { useEffect } from "react";
+import { divIcon } from "leaflet";
+import { useMemo } from "react";
 
 type UserMarkerProps = {
   latitude: number;
@@ -11,24 +11,43 @@ type UserMarkerProps = {
 
 /**
  * UserMarker component - Displays a marker at the user's location
- * Fixes the default marker icon issue with Next.js/Leaflet
+ * Uses a custom blue divIcon to distinguish from hotspot markers
  */
 export default function UserMarker({ latitude, longitude }: UserMarkerProps) {
-  useEffect(() => {
-    // Fix for default marker icon issue in Next.js
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    delete (Icon.Default.prototype as any)._getIconUrl;
-    Icon.Default.mergeOptions({
-      iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-      iconRetinaUrl:
-        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-      shadowUrl:
-        "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  // Create a custom blue marker icon for user location
+  const userIcon = useMemo(() => {
+    return divIcon({
+      className: "user-location-marker",
+      html: `
+        <div style="
+          width: 24px;
+          height: 24px;
+          background-color: #3b82f6;
+          border: 3px solid white;
+          border-radius: 50%;
+          box-shadow: 0 2px 8px rgba(0,0,0,0.3);
+          position: relative;
+        ">
+          <div style="
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 12px;
+            height: 12px;
+            background-color: white;
+            border-radius: 50%;
+          "></div>
+        </div>
+      `,
+      iconSize: [24, 24],
+      iconAnchor: [12, 12],
+      popupAnchor: [0, -12],
     });
   }, []);
 
   return (
-    <Marker position={[latitude, longitude]}>
+    <Marker position={[latitude, longitude]} icon={userIcon}>
       <Popup>
         <div className="text-center">
           <strong>Your Location</strong>
